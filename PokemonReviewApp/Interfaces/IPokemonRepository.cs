6aecda4a1ee0824e55d4ff4b07010c01af90d80c
Ali <amadams12336@gmail.com>
2022-10-17 17:34:13 -0500

    added SQL server, set up a controller, and db context

--- PokemonReviewApp/Interfaces/IPokemonRepository.cs
+++ PokemonReviewApp/Interfaces/IPokemonRepository.cs
@@ -5,6 +5,10 @@ namespace PokemonReviewApp.Interfaces
     public interface IPokemonRepository
     {
         ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeId);
 
     }
 }
