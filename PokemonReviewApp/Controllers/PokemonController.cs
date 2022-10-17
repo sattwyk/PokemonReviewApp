e260bb9d9e8f9ded52ab760968026c8c2b564b0d
Ali <amadams12336@gmail.com>
2022-10-17 17:49:41 -0500

    added automapper, set up the rest of pokemon controller

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -27,5 +27,38 @@ namespace PokemonReviewApp.Controllers
             return Ok(pokemons);
         }
 
+
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        public IActionResult GetPokemon(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))
+                return NotFound();
+
+            var pokemon = _pokemonRepository.GetPokemon(pokeId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemon);
+        }
+
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))
+                return NotFound();
+
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(rating);
+
+        }
     }
 }
