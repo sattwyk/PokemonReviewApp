f02b87b97742eaf729d1edb6689c4466b357b2e2
Ali <amadams12336@gmail.com>
2022-10-18 14:59:01 -0500

    added owner controller

---
+++ PokemonReviewApp/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        //ICollection is editable collection of Ienumerable
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int ownerId);
+        ICollection<Owner> GetOwnerOfApokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+        bool OwnerExists(int ownerId);
+    }
+}
