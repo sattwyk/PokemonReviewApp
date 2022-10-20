0ea83a64bee23679f8b3e9765041a108c6179692
Ali <amadams12336@gmail.com>
2022-10-19 20:26:49 -0500

    adding post functions to the pokemon controller

--- PokemonReviewApp/Interfaces/IPokemonRepository.cs
+++ PokemonReviewApp/Interfaces/IPokemonRepository.cs
@@ -9,6 +9,8 @@ namespace PokemonReviewApp.Interfaces
         Pokemon GetPokemon(string name);
         decimal GetPokemonRating(int pokeId);
         bool PokemonExists(int pokeId);
+        bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon);
+        bool Save();
 
     }
 }
